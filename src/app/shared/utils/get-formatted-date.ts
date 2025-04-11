d3fcea3266612188ccdc8dd2d211765df111532f
Edgar Carrasco <edgarcarrascob06@gmail.com>
2025-04-11 12:59:19 -0500

    resolve

--- src/app/shared/utils/get-formatted-date.ts
+++ src/app/shared/utils/get-formatted-date.ts
@@ -1,8 +1,13 @@
 import moment from "moment";
-import { Timestamp } from "@ncl/app/shared/types";
+import { Timestamp } from "firebase/firestore";
 
-export const getFormattedDate = (timestamp?: Timestamp) => {
-  return timestamp
-    ? moment(new Date(timestamp._seconds * 1000)).format("DD/MM/YYYY")
-    : "-";
+export const getFormattedDate = (
+  timestamp?: Timestamp | { _seconds: number; _nanoseconds: number },
+) => {
+  if (timestamp && "_seconds" in timestamp) {
+    const date = new Date(timestamp._seconds * 1000);
+    return moment(date).format("DD/MM/YYYY");
+  }
+
+  return "-";
 };
